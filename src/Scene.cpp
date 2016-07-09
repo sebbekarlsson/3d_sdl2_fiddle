b444c429689c26b4f152577d4114f4866398de96
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-07-09 21:38:33 +0200

    Added instantiate function on scene

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -5,3 +5,7 @@ Scene::Scene () {
     this->initialized = false;
     this->camera = new Camera(0.0f, 8.0f, 24.0f);
 }
+
+void Scene::instantiate(Instance * instance) {
+    this->instances.push_back(instance);
+}
--- src/Scene.h
+++ src/Scene.h
@@ -1,30 +1,24 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_opengl.h>
+#include <vector>
 #include "Camera.h"
 #pragma once
 #ifndef SCENE_H
 #define SCENE_H
 
 
+using namespace std;
+
 class Scene {
     public:
         Camera *camera;
         bool initialized;
+        std::vector<Instance *> instances;
 
         Scene();
 
-        /**
-         * This function is used to draw all instances in this scene.
-        */
-        void drawInstances(float delta) {
-        }
-
-        /**
-         * This function is used to tick all instances in this scene.
-        */
-        void tickInstances(float delta) {
-        }
+        void instantiate(Instance * instance);
 
         virtual void initialize(float delta) = 0;
         virtual void tick(float delta) = 0;
